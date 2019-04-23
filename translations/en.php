cc47432f72c46b5b3531e02e3ae2c18cff6064e8
Toni Cortés <info@tonicort.es>
2019-04-23 17:32:40 +0100

    Add english translations

---
+++ translations/en.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{factocreateoverride}prestashop>factocreateoverride_92ab2adda8a23099cabd6df0aa7c41a8'] = 'Override file creator';
+$_MODULE['<{factocreateoverride}prestashop>factocreateoverride_6919618a6b35c92f9fd7ed58e4772248'] = ' With this module you can create files in the Override folder';
+$_MODULE['<{factocreateoverride}prestashop>configure_92ab2adda8a23099cabd6df0aa7c41a8'] = 'Override file creator';
+$_MODULE['<{factocreateoverride}prestashop>configure_5b8550fb4c54193bc549923c83042baf'] = 'This is a module created by factoríadigital!';
+$_MODULE['<{factocreateoverride}prestashop>configure_1a0150117e807caf279cde8bdf66f406'] = ' Configuration';
+$_MODULE['<{factocreateoverride}prestashop>configure_5793740ca4d5198339014326824359c9'] = ' To create the necessary files in the override folder, click on the following link';
+$_MODULE['<{factocreateoverride}prestashop>configure_74a4fac83fc848d3bf66057e1579b2da'] = 'Create override files';
